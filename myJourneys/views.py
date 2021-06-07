e2fff438e135810e73d3467c7fbda1189d71ad96
Elijah Rose <elijahr0001@gmail.com>
2021-06-07 19:03:40 +0100

    add dashboard template

--- myJourneys/views.py
+++ myJourneys/views.py
@@ -14,7 +14,19 @@ def index(request):
          'vehicle_id': vehicle_id,
     }
     return render(request, 'index.html', context=context)
-    # return HttpResponse("Bruh")
+
+def dashboard(request):
+
+    # v = Vehicle(vehicle_id = 1, type= CarChoice.TUK, capacity= 3, license_plate_number= "hjdhwhgdhjw")
+    # vehicle_type = v.type.name
+    # vehicle_id = v.vehicle_id
+
+    context = {
+        # 'vehicle_type': vehicle_type,
+        #  'vehicle_id': vehicle_id,
+    }
+    return render(request, 'dashboard.html', context=context)
+
 
 def index_redirect(request):
     return HttpResponsePermanentRedirect('/myJourneys/')
