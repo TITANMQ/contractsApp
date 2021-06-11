d18f2ef27d55fa9b73fbc0f04d40d33ed151af49
Elijah Rose <elijahr0001@gmail.com>
2021-06-11 21:02:39 +0100

    Add form functionality to driver registration

--- myJourneys/views.py
+++ myJourneys/views.py
@@ -1,8 +1,8 @@
 from django.http.response import HttpResponse, HttpResponsePermanentRedirect
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Customer, Vehicle, CarChoice
-from .forms import RegisterForm
+from .models import Customer, Driver, Vehicle, CarChoice, User
+from .forms import DriverRegisterForm, RegisterForm
 
 
 # Create your views here.
@@ -10,6 +10,18 @@ def index(request):
     context = {
 
     }
+
+
+    driver = Driver.objects.get(user_id= 2)
+
+    print(driver.username)
+    print(driver.vehicle.type)
+    print(driver.vehicle.capacity)
+    print(driver.vehicle.license_plate_number)
+
+    # vehicle = Vehicle.objects.get(vehicle_id= 1)
+    # vehicle.delete()
+
     return render(request, 'index.html', context=context)
 
 
@@ -48,6 +60,7 @@ def register(request):
         if form.is_valid():
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
             email = form.cleaned_data['email']
             account_type = form.cleaned_data['account_type']
 
@@ -55,16 +68,31 @@ def register(request):
             confirm_password = form.cleaned_data['confirm_password']
 
             if password == confirm_password:
-                print(first_name, last_name, email, account_type)
+                # print(first_name, last_name, email, account_type)
 
+                request.session['account_type'] = account_type.lower()
                 
                 if account_type == 'DRIVER':
+
+                    driver = Driver(username=username,  first_name = first_name, last_name=last_name, email_address=email, password=password)
+                    vehicle = Vehicle()
+                    vehicle.save()
+                    driver.vehicle = vehicle
+                    driver.save()
+                    request.session['user_id'] = driver.user_id 
                     return HttpResponseRedirect('/myJourneys/register/driver')
                 else:
-                    cust = Customer(username='test2',  first_name = first_name, last_name=last_name, email_address=email, password=password)
+                    cust = Customer(username=username,  first_name = first_name, last_name=last_name, email_address=email, password=password)
                     cust.save()
-                    print(Customer.objects.get(user_id=1).first_name)
-                    return HttpResponseRedirect('/myJourneys/register/successful')
+                    request.session['user_id'] = cust.user_id 
+
+                    if Customer.objects.get(user_id=cust.user_id).username == cust.username:
+                        print(Customer.objects.get(user_id=cust.user_id).username)
+                    else:
+
+                        print('database error')
+    
+                    return HttpResponsePermanentRedirect('/myJourneys/register/successful')
 
                 return
         else:
@@ -77,13 +105,43 @@ def register(request):
 
 
 def register_driver(request):
-    context = {}
+    
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = DriverRegisterForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            vehicle_type = form.cleaned_data['vehicle_type']
+            capacity= form.cleaned_data['capacity']
+            license_plate = form.cleaned_data['license_plate']
 
-    return render(request, 'register_driver.html', context=context)
+        
+            user_id = request.session['user_id'] 
+
+            driver = Driver.objects.get(user_id=user_id)
+            print(driver.username)
+
+            vehicle = driver.vehicle
+            vehicle.type = vehicle_type
+            vehicle.capacity = capacity
+            vehicle.license_plate_number = license_plate
+
+            vehicle.save()
+            
+            return HttpResponseRedirect('/myJourneys/register/successful')
+            
+    
+    else:
+        form = DriverRegisterForm()
+
+    return render(request, 'register_driver.html', {'form': form})
 
 
 def register_success(request):
-    context = {}
+
+    account_type = request.session['account_type'] # Get the account type
+
+    context = {'account_type': account_type }
 
     return render(request, 'registration_successful.html', context=context)
 
